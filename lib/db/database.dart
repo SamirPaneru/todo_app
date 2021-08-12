9eb8bc38d6af67a7e5660a432a4b21c04ced96f2
SamirPaneru <sarthak.paneru007@gmail.com>
2021-08-12 18:08:19 +0545

    Initailised and closed database

--- lib/db/database.dart
+++ lib/db/database.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:sqflite/sqflite.dart';
 import 'package:todo_app/model/todo.dart';
 import 'package:path/path.dart';
@@ -14,5 +16,26 @@ class TodoDatabase {
     // we add ! if we are sure _database or any variable will never be null..
     // https://stackoverflow.com/questions/66860060/in-flutter-dart-why-do-we-sometimes-add-a-question-mark-to-string-when-decl
     if (_database != null) return _database!;
+
+    _database = await _initDB('todo.db');
+    return _database!;
+  }
+
+  // Initializing database
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  // Create table
+  FutureOr<void> _createDB(Database db, int version) async {}
+
+  // Close datadase
+  Future close() async {
+    final db = await instance.database;
+
+    db.close();
   }
 }
